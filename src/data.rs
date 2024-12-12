010c7d4666e0a65ffb377b45fcb4b221154f164a
BhavikaPathria-02 <path0053@algonquinlive.com>
2024-12-12 18:35:31 -0500

    Commit

--- src/data.rs
+++ src/data.rs
@@ -5,73 +5,25 @@ pub fn fetch_products(_settings: &Settings) -> Vec<Product> {
     vec![
         Product {
             id: 1,
-            name: "Contoso Catnip's Friend".to_string(),
-            price: 9.99,
-            description: "Watch your feline friend embark on a fishing adventure with Contoso Catnip's Friend toy. Packed with irresistible catnip and dangling fish lure.".to_string(),
-            image: "/catnip.jpg".to_string()
+            name: "Iphone 14 Pro Max".to_string(),
+            price: 120.99,
+            description: "The iPhone 14 Pro Max pushes the boundaries of innovation with its advanced A-series chip, delivering unmatched speed and efficiency. It features a cutting-edge ProMotion display for smooth visuals and a dynamic triple-camera system designed to capture stunning photos and videos in any light. Wrapped in a sleek, robust design, this phone offers an immersive user experience with improved battery life and enhanced privacy features.".to_string(),
+            image: "/BestBuy-Iphone.png".to_string()
         },
         Product {
             id: 2,
-            name: "Salty Sailor's Squeaky Squid".to_string(),
-            price: 6.99,
-            description: "Let your dog set sail with the Salty Sailor's Squeaky Squid. This interactive toy provides hours of fun, featuring multiple squeakers and crinkle tentacles.".to_string(),
-            image: "/squid.jpg".to_string()
+            name: "Wireless Mouse".to_string(),
+            price: 26.99,
+            description: "The wireless mouse provides a clutter-free workspace by eliminating the need for cables, offering freedom and flexibility with its robust wireless connectivity. It features an ergonomic design for comfortable use during extended periods, and adjustable DPI settings for precision tracking. Equipped with long-lasting batteries and compatible with multiple operating systems, it’s ideal for both office and personal use.".to_string(),
+            image: "/WirelessMouse-BestBuy.png".to_string()
         },
         Product {
             id: 3,
-            name: "Mermaid's Mice Trio".to_string(),
-            price: 12.99,
-            description: "Entertain your kitty with the Mermaid's Mice Trio. These adorable plush mice are dressed as mermaids and filled with catnip to captivate their curiosity.".to_string(),
-            image: "/mermaid.jpg".to_string()
-        },
-        Product {
-            id: 4,
-            name: "Ocean Explorer's Puzzle Ball".to_string(),
-            price: 11.99,
-            description: "Challenge your pet's problem-solving skills with the Ocean Explorer's Puzzle Ball. This interactive toy features hidden compartments and treats, providing mental stimulation and entertainment.".to_string(),
-            image: "/ocean.jpg".to_string()
-        },
-        Product {
-            id: 5,
-            name: "Pirate Parrot Teaser Wand".to_string(),
-            price: 8.99,
-            description: "Engage your cat in a playful pursuit with the Pirate Parrot Teaser Wand. The colorful feathers and jingling bells mimic the mischievous charm of a pirate's parrot.".to_string(),
-            image: "/pirate.jpg".to_string()
-        },
-        Product {
-            id: 6,
-            name: "Seafarer's Tug Rope".to_string(),
-            price: 14.99,
-            description: "Tug-of-war meets nautical adventure with the Seafarer's Tug Rope. Made from marine-grade rope, it's perfect for interactive play and promoting dental health in dogs.".to_string(),
-            image: "/tug.jpg".to_string()
-        },
-        Product {
-            id: 7,
-            name: "Seashell Snuggle Bed".to_string(),
-            price: 19.99,
-            description: "Give your furry friend a cozy spot to curl up with the Seashell Snuggle Bed. Shaped like a seashell, this plush bed provides comfort and relaxation for cats and small dogs.".to_string(),
-            image: "/bed.jpg".to_string()
-        },
-        Product {
-            id: 8,
-            name: "Nautical Knot Ball".to_string(),
-            price: 7.99,
-            description: "Unleash your dog's inner sailor with the Nautical Knot Ball. Made from sturdy ropes, it's perfect for fetching, tugging, and satisfying their chewing needs.".to_string(),
-            image: "/knot.jpg".to_string()
-        },
-        Product {
-            id: 9,
-            name: "Contoso Claw's Crabby Cat Toy".to_string(),
-            price: 3.99,
-            description: "Watch your cat go crazy for Contoso Claw's Crabby Cat Toy. This crinkly and catnip-filled toy will awaken their hunting instincts and provide endless entertainment.".to_string(),
-            image: "/crabby.jpg".to_string()
-        },
-        Product {
-            id: 10,
-            name: "Ahoy Doggy Life Jacket".to_string(),
-            price: 5.99,
-            description: "Ensure your furry friend stays safe during water adventures with the Ahoy Doggy Life Jacket. Designed for dogs, this flotation device offers buoyancy and visibility in style.".to_string(),
-            image: "/lifejacket.jpg".to_string()
+            name: "Dell Laptop".to_string(),
+            price: 559.99,
+            description: "The Dell Laptop combines powerful performance with high-end design, featuring the latest Intel processors and crisp, vibrant displays for an optimal viewing experience. It comes equipped with a durable battery, ample storage space, and advanced connectivity options to cater to both business professionals and casual users. Lightweight and elegant, this laptop offers reliability and ease of use, making it a top choice for everyday computing tasks.".to_string(),
+            image: "/BestBuy-LaptopDell.png".to_string()
         }
+       
     ]
 }
